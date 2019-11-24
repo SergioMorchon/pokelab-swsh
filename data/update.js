6de7443bc82daeba40c9e081c581d277a8b4dd24
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2019-11-24 21:07:28 +0100

    Add json artifacts

--- data/update.js
+++ data/update.js
@@ -1,7 +1,14 @@
-import { writeFileSync } from "fs";
+import { writeFileSync, existsSync, mkdirSync } from "fs";
 import { sword_shield_stats } from "./sword-shield-stats.js";
+import { sep } from "path";
+
+const jsonPath = [process.cwd(), "data", "json"].join(sep) + sep;
+
+if (!existsSync(jsonPath)) {
+  mkdirSync(jsonPath);
+}
 
 writeFileSync(
-  "./data/sword_shield_stats.json",
+  `${jsonPath}sword_shield_stats.json`,
   JSON.stringify(sword_shield_stats, null, "\t")
 );
